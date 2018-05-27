11726506ddcf9435498e4cf807c8e82230f7c9df
anasmatic <anasmatic@gmail.com>
2018-05-28 04:18:41 +0800

    init code

---
+++ helper.py
@@ -0,0 +1,200 @@
+import re
+import random
+import numpy as np
+import os.path
+import scipy.misc
+import shutil
+import zipfile
+import time
+import tensorflow as tf
+from glob import glob
+from urllib.request import urlretrieve
+from tqdm import tqdm
+import cv2
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+
+def maybe_download_pretrained_vgg(data_dir):
+    """
+    Download and extract pretrained vgg model if it doesn't exist
+    :param data_dir: Directory to download the model to
+    """
+    vgg_filename = 'vgg.zip'
+    vgg_path = os.path.join(data_dir, 'vgg')
+    vgg_files = [
+        os.path.join(vgg_path, 'variables/variables.data-00000-of-00001'),
+        os.path.join(vgg_path, 'variables/variables.index'),
+        os.path.join(vgg_path, 'saved_model.pb')]
+
+    missing_vgg_files = [vgg_file for vgg_file in vgg_files if not os.path.exists(vgg_file)]
+    if missing_vgg_files:
+        # Clean vgg dir
+        if os.path.exists(vgg_path):
+            shutil.rmtree(vgg_path)
+        os.makedirs(vgg_path)
+
+        # Download vgg
+        print('Downloading pre-trained vgg model...')
+        with DLProgress(unit='B', unit_scale=True, miniters=1) as pbar:
+            urlretrieve(
+                'https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip',
+                os.path.join(vgg_path, vgg_filename),
+                pbar.hook)
+
+        # Extract vgg
+        print('Extracting model...')
+        zip_ref = zipfile.ZipFile(os.path.join(vgg_path, vgg_filename), 'r')
+        zip_ref.extractall(data_dir)
+        zip_ref.close()
+
+        # Remove zip file to save space
+        os.remove(os.path.join(vgg_path, vgg_filename))
+
+def preprocess_labels(label_image):
+    #exclude labels : 1,2,3,5,8,9,11,12
+    labels_new = np.copy(label_image)
+    # Identify lane marking pixels (label is 6)
+    lane_marking_pixels = (label_image[:,:,0] == 6).nonzero()
+    #lane_marking_pixels = np.zeros_like(label_image[:,:,0])
+    #print(lane_marking_pixels)
+    #lane_marking_pixels[label_image[:,:,0] == 6 ] = 1
+    #print(lane_marking_pixels)
+    # Set lane marking pixels to road (label is 7)
+    labels_new[lane_marking_pixels] = 7
+    # Identify all vehicle pixels
+    vehicle_pixels = (label_image[:,:,0] == 10).nonzero()
+    # Isolate vehicle pixels associated with the hood (y-position > 496)
+    hood_indices = (vehicle_pixels[0] >= 496).nonzero()[0]
+    hood_pixels = (vehicle_pixels[0][hood_indices], \
+                   vehicle_pixels[1][hood_indices])
+    
+    # Set hood pixel labels to 0
+    labels_new[hood_pixels] = 0
+    
+    other_pixels = (label_image[:,:,0] == 1).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 2).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 3).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 5).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 8).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 9).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 11).nonzero()
+    labels_new[other_pixels] = 0
+    other_pixels = (label_image[:,:,0] == 12).nonzero()
+    labels_new[other_pixels] = 0
+    # Return the preprocessed label image 
+    return labels_new
+
+def gen_batch_function(data_folder, image_shape):
+    """
+    Generate function to create batches of training data
+    :param data_folder: Path to folder that contains all the datasets
+    :param image_shape: Tuple - Shape of image
+    :return:
+    """
+    def get_batches_fn(batch_size):
+        """
+        Create batches of training data
+        :param batch_size: Batch Size
+        :return: Batches of training data
+        """
+        image_paths = glob(os.path.join(data_folder, 'CameraRGB', '*.png'))
+        label_paths = glob(os.path.join(data_folder, 'CameraSeg', '*.png'))
+        background_color = np.array([255, 0, 0])
+        random.shuffle(image_paths)
+        for batch_i in range(0, len(image_paths), batch_size):
+            images = []
+            gt_images = []
+            for image_file in image_paths[batch_i:batch_i+batch_size]:
+                gt_image_file = image_file.replace("CameraRGB", "CameraSeg")
+
+                #image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                #gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+                image = scipy.misc.imread(image_file)
+                gt_image = scipy.misc.imread(gt_image_file)
+
+                #gt_bg = np.all(gt_image == background_color, axis=2)
+                #gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                #gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+                gt_image = preprocess_labels(gt_image)
+                gt_image = gt_image.reshape(*gt_image.shape, 1)
+                images.append(image)
+                gt_images.append(gt_image)
+                #augment ---------------------------
+                #  1- grayscal with contrast
+                img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+                img = cv2.equalizeHist(img)#contrast http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_equalization/histogram_equalization.html
+                cv2.normalize(img,img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)#, dtype=cv2.CV_32F)
+                #img.shape = (img.shape[0],img.shape[1],3)#update shape
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+                images.append(img)
+                gt_images.append(gt_image)
+                '''#  2-flip grayscale and original
+                img_h_flip = cv2.flip( img, 0 )
+                image_h_flip = cv2.flip( image, 0 )
+                try:
+                    gt_image_h_flip = cv2.flip( gt_image, 0 )
+                except:
+                    gt_image_h_flip = gt_image    
+                images.append(img_h_flip)
+                gt_images.append(gt_image_h_flip)
+                images.append(image_h_flip)
+                gt_images.append(gt_image_h_flip)
+                '''
+                #augment end ----------------------------------------------------
+            yield np.array(images), np.array(gt_images)
+    return get_batches_fn
+
+
+def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
+    """
+    Generate test output using the test images
+    :param sess: TF session
+    :param logits: TF Tensor for the logits
+    :param keep_prob: TF Placeholder for the dropout keep robability
+    :param image_pl: TF Placeholder for the image placeholder
+    :param data_folder: Path to the folder that contains the datasets
+    :param image_shape: Tuple - Shape of image
+    :return: Output for for each test image
+    """
+    for image_file in glob(os.path.join(data_folder, 'image_2', '*.png')):
+        image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+
+        im_softmax = sess.run(
+            [tf.nn.softmax(logits)],
+            {keep_prob: 1.0, image_pl: [image]})
+        im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+        segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
+        mask = scipy.misc.toimage(mask, mode="RGBA")
+        street_im = scipy.misc.toimage(image)
+        street_im.paste(mask, box=None, mask=mask)
+
+        yield os.path.basename(image_file), np.array(street_im)
+
+
+def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image):
+    # Make folder for current run
+    output_dir = os.path.join(runs_dir, str(time.time()))
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    # Run NN on test images and save them to HD
+    print('Training Finished. Saving test images to: {}'.format(output_dir))
+    image_outputs = gen_test_output(
+        sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
+    for name, image in image_outputs:
+        scipy.misc.imsave(os.path.join(output_dir, name), image)
---
+++ main.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+import os.path
+import tensorflow as tf
+import helper
+import warnings
+from distutils.version import LooseVersion
+import numpy as np
+import cv2
+import IPython
+
+# Check TensorFlow Version
+assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+print('TensorFlow Version: {}'.format(tf.__version__))
+# Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+    
+def load_vgg(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    """
+    # TODO: Implement function
+    #   Use tf.saved_model.loader.load to load the model and weights
+    vgg_tag = 'vgg16'
+    vgg_input_tensor_name = 'image_input:0'
+    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_layer3_out_tensor_name = 'layer3_out:0'
+    vgg_layer4_out_tensor_name = 'layer4_out:0'
+    vgg_layer7_out_tensor_name = 'layer7_out:0'
+    #load graph from file
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    #grap the graph in variabel
+    graph = tf.get_default_graph()
+    #get desired layers by name
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    out_3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    out_4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    out_7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return w1, keep, out_3, out_4, out_7
+
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+    """
+    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
+    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
+    :param num_classes: Number of classes to classify
+    :return: The Tensor for the last layer of output
+    """
+    conv_1x1_7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1,1), padding='same',
+                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                  kernel_initializer=tf.truncated_normal_initializer(stddev=1e-3))#https://www.programcreek.com/python/example/90502/tensorflow.truncated_normal_initializer
+    conv_1x1_4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, strides=(1,1), padding='same',
+                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                  kernel_initializer=tf.truncated_normal_initializer(stddev=1e-3))
+    conv_1x1_3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, strides=(1,1), padding='same',
+                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                  kernel_initializer=tf.truncated_normal_initializer(stddev=1e-3))
+    transpose_7 = tf.layers.conv2d_transpose(conv_1x1_7, num_classes, 4, strides=(2, 2),padding='same',
+                                             kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                             kernel_initializer=tf.truncated_normal_initializer(stddev=1e-3))
+    add_7_4 = tf.add(transpose_7, conv_1x1_4)
+    transpose_4 = tf.layers.conv2d_transpose(add_7_4, num_classes, 4, strides=(2, 2),padding='same',
+                                             kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                             kernel_initializer=tf.truncated_normal_initializer(stddev=1e-3))
+    add_4_3 = tf.add(transpose_4 , conv_1x1_3 )
+    transpose_3 = tf.layers.conv2d_transpose(add_4_3, num_classes, 16, strides=(8, 8),padding='same',
+                                             kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                             kernel_initializer=tf.truncated_normal_initializer(stddev=1e-3))
+    return transpose_3
+
+def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
+    """
+    Build the TensorFLow loss and optimizer operations.
+    :param nn_last_layer: TF Tensor of the last layer in the neural network
+    :param correct_label: TF Placeholder for the correct label image
+    :param learning_rate: TF Placeholder for the learning rate
+    :param num_classes: Number of classes to classify
+    :return: Tuple of (logits, train_op, cross_entropy_loss)
+    """
+    logits = tf.reshape(nn_last_layer, (-1, num_classes), name='logits')
+    labels = tf.reshape(correct_label, (-1, num_classes), name='labels')
+    print("~~~~logits~~~~")
+    print(logits)
+    print("----labels----")
+    print(labels)
+    print("_________")
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels,logits=logits))
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss = cross_entropy_loss);
+    return logits, train_op, cross_entropy_loss
+
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+             correct_label, keep_prob, learning_rate):
+    """
+    Train neural network and print out the loss during training.
+    :param sess: TF Session
+    :param epochs: Number of epochs
+    :param batch_size: Batch size
+    :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
+    :param train_op: TF Operation to train the neural network
+    :param cross_entropy_loss: TF Tensor for the amount of loss
+    :param input_image: TF Placeholder for input images
+    :param correct_label: TF Placeholder for label images
+    :param keep_prob: TF Placeholder for dropout keep probability
+    :param learning_rate: TF Placeholder for learning rate
+    """
+    
+    for epoch in range(epochs):
+        print(" ----- starting epoch %d , batchsize=%d"%(epoch,batch_size))
+        batch_num = 0
+        batch_total_loss = 0
+        for image, label in get_batches_fn(batch_size):
+            #image = np.reshape(image, (-1,-1,-1,num))
+            train , batch_loss = sess.run([train_op,cross_entropy_loss], 
+                     feed_dict={input_image:image,correct_label:label,keep_prob:0.5,learning_rate:0.001})
+            batch_total_loss = batch_total_loss + batch_loss
+            print("    batch %d has loss %f"%(batch_num,batch_loss))
+            batch_num +=1
+        epoch_loss=batch_total_loss/batch_num
+        print ("  epoch %d finished with loss %f"%(epoch,epoch_loss))
+    pass
+
+def run():
+    num_classes = 4
+    image_shape = (600, 800)
+    data_dir = './data'
+    train_dir = 'Train'
+    runs_dir = './runs'
+    #tests.test_for_kitti_dataset(data_dir)
+
+    # Download pretrained vgg model
+    helper.maybe_download_pretrained_vgg(data_dir)
+
+    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
+    # You'll need a GPU with at least 10 teraFLOPS to train on.
+    #  https://www.cityscapes-dataset.com/
+    epochs = 10#2#10
+    batch_size = 6
+    #learning_rate = 10.0#from project_tests.py
+    with tf.Session() as sess:
+        #vars
+        correct_label = tf.placeholder(tf.float32, [None, None, None, num_classes], name='correct_label')#from project_tests.py
+        #correct_label = np.reshape(correct_label, (-1, image_shape[0], image_shape[1], 2))
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')#from project_tests.py
+            
+        # Path to vgg model
+        vgg_path = os.path.join(data_dir, 'vgg')
+        # Create function to get batches
+        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, train_dir), image_shape)
+
+        # OPTIONAL: Augment Images for better results
+        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+		
+        # TODO: Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess,vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(layer_output,correct_label,learning_rate,num_classes)
+        
+        sess.run(tf.global_variables_initializer())
+        # TODO: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+             correct_label, keep_prob, learning_rate)
+        
+        #TODO: safe model : 
+        saver = tf.train.Saver()
+        saver.save(sess, './saved/segmentation_model_180527')
+        print("Model Saved!")
+        # TODO: Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+
+
+if __name__ == '__main__':
+    run()
