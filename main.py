ee5a77e5a15d1bc834c63cdec15f8d791127a25b
anasmatic <anasmatic@gmail.com>
2018-05-28 13:57:07 +0800

    drawback to tf 1.3
    use old softmax_cross_entropy_with_logits instead of softmax_cross_entropy_with_logits_v2

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     print("----labels----")
     print(labels)
     print("_________")
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels,logits=logits))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=logits))
     train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss = cross_entropy_loss);
     return logits, train_op, cross_entropy_loss
 
